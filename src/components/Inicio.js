b948aeb8d52613dd543536ee7bc8b1a63e92b291
Esteban Velardez <estebanbvelardez@gmail.com>
2021-04-14 17:12:23 -0300

    se agrego el componente navegacion y footer

---
+++ src/components/Inicio.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import banner from './img/coffee.jpg'
+
+const Inicio = () => {
+  return (
+    <Fragment>
+     <img src={banner} alt="fonde de la cafeteria" className='w-100'/>
+    </Fragment>
+  );
+};
+
+export default Inicio;

6bfb4e3c8d655fe73775fe38d9b2a0f923f713b3
Esteban Velardez <estebanbvelardez@gmail.com>
2021-04-26 17:18:17 -0300

    se agrego la logica para eliminar un producto

--- src/components/ItemProductos.js
+++ src/components/ItemProductos.js
@@ -1,21 +1,71 @@
-import React from 'react';
-import {ListGroup, Button} from 'react-bootstrap';
-
+import React from "react";
+import { ListGroup, Button } from "react-bootstrap";
+import Swal from "sweetalert2";
 
 const ItemProductos = (props) => {
-    return (
+  const eliminarProducto = (codigo) => {
+    Swal.fire({
+      title: "¿Estas seguro de eliminar el producto?",
+      text: "No puedes recuperar un producto eliminado",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Eliminar",
+      cancelButtonText: "Cancelar",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        console.log(codigo);
+        //Agregar solicitud deleted
+        try {
+          const URL = process.env.REACT_APP_API_URL + "/" + codigo;
+          const respuesta = await fetch(URL, {
+            method: "DELETE",
+            headers: {
+              "Content-type": "application/json",
+            },
+          });
+
+          console.log(respuesta);
+          if (respuesta.status === 200) {
+            Swal.fire(
+              "Producto eliminado!",
+              "El producto seleccionado fue correctamente eliminado",
+              "success"
+            );
+
+            // Volver a hacer la consula a la API
+            props.consultarAPI();
+          }
+        } catch (error) {
+          console.log(error);
+          Swal.fire("Ocurrio un error", "Intentelo nuevamente", "warning");
+        }
+      }
+    });
+  };
+
+  return (
+    <div>
+      <ListGroup.Item className="d-flex justify-content-between">
+        <p>
+          {props.producto.nombreProducto}{" "}
+          <span className="font-weight-bold">
+            ${props.producto.precioProducto}
+          </span>
+        </p>
         <div>
-              <ListGroup.Item className='d-flex justify-content-between'>
-                  <p>
-    {props.producto.nombreProducto} <span className='font-weight-bold'>${props.producto.precioProducto}</span> 
-                  </p>
-                  <div>
-              <Button variant='warning mr-2'>Editar</Button>
-              <Button variant='danger'>Borrar</Button>
-                  </div>
-              </ListGroup.Item>
+          <Button variant="warning mr-2">Editar</Button>
+          <Button
+            variant="danger"
+            onClick={() => eliminarProducto(props.producto.id)}
+          >
+            Borrar
+          </Button>
         </div>
-    );
+      </ListGroup.Item>
+    </div>
+  );
 };
 
 export default ItemProductos;
